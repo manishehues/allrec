07009d40e3d0d886dda4006c25e9dc9721144a83
Dez <dez@gmail.com>
2022-03-15 17:57:29 +0530

    slider and rev

--- wp-content/themes/twentytwentyone-child/templates/homepage.php
+++ wp-content/themes/twentytwentyone-child/templates/homepage.php
@@ -14,7 +14,8 @@ get_header();
 <div class="homePage container">
     <div class="heroBanner">
         <div class="videoSection">
-            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/Videoimg.png"/>
+            <!-- <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/Videoimg.png"/> -->
+            <script src="https://fast.wistia.com/embed/medias/oqq38tahjn.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_oqq38tahjn videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/oqq38tahjn/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>
         </div>
         <div class="heroContent">
             <div class="content">
@@ -46,15 +47,15 @@ get_header();
                 a higher chance of winning.</p>
             </div>
             <div class="sildeSlider">
-                <ul>
+                <!-- <ul>
                     <li><a href="#">0</a></li>
                     <li><a href="#">0</a></li>
                     <li><a class="active" href="#">0</a></li>
-                </ul>
+                </ul> -->
             </div>
         </div>
         <div class="ProductShortcode">       
-            <?php echo do_shortcode('[products]'); ?>
+            <?php echo do_shortcode('[wcpscwc_pdt_slider type="products"]'); ?>
         </div>
     </div>
 
