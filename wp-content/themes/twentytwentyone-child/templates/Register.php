24d773d4cb4bf97f27288c940745193eed9ace40
manish <manish@ehues.com>
2022-04-12 11:56:00 +0530

    new changes

---
+++ wp-content/themes/twentytwentyone-child/templates/Register.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Register
+ *
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+
+<div class="registerCustom">
+    <div class="shortContainer">
+        <div class="formlogo">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/allreclog.svg"/>
+        </div>
+        <?php echo do_shortcode( '[arm_setup id="1"]' ); ?>
+    </div>
+</div>ghf
+
+
+
+<?php
+get_footer();
